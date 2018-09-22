cbfd571aa1db418b7174ea6cca109cf7dd74718a
Alex <alex.varghese77@gmail.com>
2018-09-22 06:07:40 -0800

    project restructured

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, } from '@angular/core';
 import { Post } from './../post.modal'
 import { PostService } from './../post.service'
-
+import {Subscription} from 'rxjs'
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit , OnDestroy {
   post: Post[] = [];
+  private postSub:Subscription;
   constructor(public postService: PostService) { }
 
   ngOnInit() {
-    this.post = this.postService.getPost()
+    this.post = this.postService.getPost();
+    this.postSub=this.postService.getPostUpdateListener().subscribe((post: Post[])=>{
+this.post=post;
+    })
+  }
+  ngOnDestroy(){
+    this.postSub.unsubscribe();
   }
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,13 +1,20 @@
 import { Post } from './post.modal'
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs'
 @Injectable({ providedIn: 'root' })
 export class PostService {
     post: Post[] = [];
+    private postUpdate=new Subject<Post[]>();
     getPost() {
         return this.post  
+    }
+
+    getPostUpdateListener(){
+        return this.postUpdate.asObservable();
     }
     addPost(title: string, contet: string) {
         let newPost: Post = { title: title, content: contet }
         this.post.push(newPost);
+        this.postUpdate.next([...this.post])
     }
 }
